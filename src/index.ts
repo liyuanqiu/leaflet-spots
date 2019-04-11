8937ff515e1721713ae45a5e21118c10133558dc
李元秋 <yuanqiu.li@wz-inc.com>
2019-04-11 11:53:45 +0800

    add comment

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,111 @@ import {
   LeafletEvent,
 } from 'leaflet';
 
+/**
+ * Use `MetadatParser` to parse user data.
+ */
 import MetadataParser from './metadata-parser';
 
+/**
+ * The spot events which will be attached to the spot.
+ * @template T User data unit
+ */
 export interface SpotEvents<T> {
   [eventName: string]: (e: LeafletEvent, metadata: T) => void;
 }
 
+/**
+ * Interactive options.
+ */
 export interface InteractiveOptions {
   filtered: boolean;
   selected: boolean;
 }
 
+/**
+ * User defined handler for interactive.
+ * @template T User data unit
+ */
 export type InteractiveHandler<T> = (metadata: T, shape: Path, options: InteractiveOptions) => void;
 
+/**
+ * The options to create the instance of `LeafletSpots`
+ * @template T User data unit
+ */
 export interface LeafletSpotsOptions<T> {
+  /**
+   * Use `MetadatParser` to parse user data.
+   * @template T User data unit
+   */
   metadataParser: MetadataParser<T>;
+  /**
+   * The spot events which will be attached to the spot.
+   * @template T User data unit
+   */
   spotEvents: SpotEvents<T>;
   /**
    * handle interactive like 'selected', 'filtered', etc
+   * @template T User data unit
    */
   handleInteractive?: InteractiveHandler<T>;
 }
 
+/**
+ * The data unit structure that `LeafletSpots` store data.
+ * @template T User data unit
+ */
 export interface SpotUnit<T> {
   metadata: T;
   shape: Path;
 }
 
+/**
+ * The data structure that `LeafletSpots` store data.
+ * @template T User data unit
+ */
 export interface Spots<T> {
   [id: string]: SpotUnit<T>;
 }
 
+/**
+ * Render engine for rendering spots on `leaflet` based system.
+ * @template T User data unit
+ */
 class LeafletSpots<T> {
+  /**
+   * A helper to parse user data T
+   * @protected
+   * @template T User data unit
+   */
   protected metadataParser: MetadataParser<T>;
+  /**
+   * Spots data storage
+   * @protected
+   * @template T User data unit
+   */
   protected spots: Spots<T>;
+  /**
+   * The layer that all the spots are rendered on
+   * @protected
+   */
   protected layer: LayerGroup;
+  /**
+   * Current selected spot's id
+   * `id` is parsed from user data by `MetadataParser`
+   * @protected
+   */
   private selected: string | null;
+  /**
+   * Spots' event
+   * @private
+   * @template T User data unit
+   */
   private spotEvents: SpotEvents<T>;
+  /**
+   * Interactive handler
+   * @private
+   * @template T User data unit
+   */
   private handleInteractive: InteractiveHandler<T>;
 
   public constructor({
@@ -58,6 +126,7 @@ class LeafletSpots<T> {
 
   /**
    * Get the layer object
+   * @returns {LayerGroup} the layer
    */
   public getLayer(): LayerGroup {
     return this.layer;
@@ -65,10 +134,12 @@ class LeafletSpots<T> {
 
   /**
    * Set new spots
+   * 1. add non-existing spots(by id)
+   * 2. update existing spots(by id)
+   * 3. remove excess spots(by id)
    * @param {T[]} spots 
    */
   public setSpots(spots: T[]): void {
-    // console.log('setSpots');
     const { parseId, parseShouldUpdate } = this.metadataParser;
     const spotsMap: {
       [id: string]: 1;
@@ -95,10 +166,11 @@ class LeafletSpots<T> {
 
   /**
    * Add a spot
+   * 1. if id existing, abort
+   * 2. add spot
    * @param {T} metadata
    */
   public addSpot(metadata: T): void {
-    // console.log('addSpot');
     const { parseId, parseShape, parseFilteration } = this.metadataParser;
     const id = parseId(metadata);
     if (this.spots[id] !== undefined) {
@@ -123,8 +195,12 @@ class LeafletSpots<T> {
     };
   }
 
+  /**
+   * Remove a spot without unselect it
+   * @private for inner usage
+   * @param {T} metadata 
+   */
   private removeSpotWithoutUnselect(metadata: T): void {
-    // console.log('removeSpotWithoutUnselected');
     const { parseId } = this.metadataParser;
     const id = parseId(metadata);
     const spotUnit = this.spots[id];
@@ -138,31 +214,34 @@ class LeafletSpots<T> {
 
   /**
    * Remove a spot
+   * 1. remove it
+   * 2. unselect it
    * @param {T} metadata
    */
   public removeSpot(metadata: T): void {
-    // console.log('removeSpot');
     this.removeSpotWithoutUnselect(metadata);
     this.unselectSpot(metadata);
   }
 
   /**
    * Update a spot
+   * 1. remove it without unselect
+   * 2. recreate a new spot
    * @param {T} metadata
    */
   public updateSpot(metadata: T): void {
-    // console.log('updateSpot');
     this.removeSpotWithoutUnselect(metadata);
     this.addSpot(metadata);
   }
 
   /**
    * handle selection
+   * 1. update `selected` member
+   * 2. update spot
    * @param {T} metadata
    * @param {boolean} selected
    */
   private handleSelection(metadata: T, selected: boolean): void {
-    // console.log('handleSelection');
     const { parseId } = this.metadataParser;
     const id = parseId(metadata);
     const spotUnit = this.spots[id];
@@ -175,10 +254,11 @@ class LeafletSpots<T> {
 
   /**
    * Select a spot
+   * 1. unselect the previous selected one
+   * 2. select this one
    * @param {T} metadata 
    */
   public selectSpot(metadata: T): void {
-    // console.log('selectSpot');
     if (this.selected !== null) {
       const { metadata } = this.spots[this.selected];
       this.unselectSpot(metadata);
@@ -191,7 +271,6 @@ class LeafletSpots<T> {
    * @param {T} metadata
    */
   public unselectSpot(metadata: T): void {
-    // console.log('unselectSpot');
     this.handleSelection(metadata, false);
   }
 
@@ -199,7 +278,6 @@ class LeafletSpots<T> {
    * Force rerender all spots
    */
   public forceRender(): void {
-    // console.log('forceRender', Object.keys(this.spots).length);
     Object.keys(this.spots).forEach((id) => {
       const { metadata } = this.spots[id];
       this.updateSpot(metadata);
